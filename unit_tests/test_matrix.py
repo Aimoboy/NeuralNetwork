e332d83a15e8db09b484ec331ba6e31f96f8da64
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2019-10-04 01:13:49 +0200

    Written some tests for the matrix class

---
+++ unit_tests/test_matrix.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.insert(1, '../')
+from matrix import Matrix
+import unittest
+
+class MultiplicationTest(unittest.TestCase):
+    def test_matrix_multiplication(self):
+        m1 = Matrix([[1, 2, 3], [4, 5, 6]])
+        m2 = Matrix([[1, 2], [3, 4], [5, 6]])
+        m_expect = Matrix([[22, 28], [49, 64]])
+        self.assertEqual(m_expect, m1 * m2)
+
+        m1 = Matrix([[-1, -2, -3], [-4, -5, -6]])
+        m2 = Matrix([[1, 2], [3, 4], [5, 6]])
+        m_expect = Matrix([[-22, -28], [-49, -64]])
+        self.assertEqual(m_expect, m1 * m2)
+
+    def test_scalar_multiplication(self):
+        m = Matrix([[1, 2, 3], [4, 5, 6]])
+        s = 10
+        m_expect = Matrix([[10, 20, 30], [40, 50, 60]])
+        self.assertEqual(m_expect, m * s)
+        self.assertEqual(m_expect, s * m)
+    
+    def test_copy(self):
+        m1 = Matrix([[1, 2, 3], [4, 5, 6]])
+        m2 = m1.copy()
+        self.assertEqual(m1, m2)
+    
+    def test_zero_matrix(self):
+        m = Matrix.zero_matrix(2, 3)
+        m_expect = Matrix([[0, 0, 0], [0, 0, 0]])
+        self.assertEqual(m_expect, m)
+
+    def test_apply_function(self):
+        f = lambda x: x * x
+        m = Matrix([[1, 2], [3, 4]])
+        m = m.apply_function(f)
+        m_expect = Matrix([[1, 4], [9, 16]])
+        self.assertEqual(m_expect, m)
